8f92029386fc301e1da186ed68410845fe032772
Raiosilva <raimundopi025@gmail.com>
2019-08-12 15:36:58 -0300

    navbar and footer

---
+++ app/assets/javascripts/sidenav.js
@@ -0,0 +1,11 @@
+$(document).on('ready turbolink:load', function() {
+  var elem, instance;
+  elem = document.querySelector('#side-nav');
+  return instance = new M.Sidenav(elem, {});
+});
+$(document).on('ready turbolinks:before-visit', function() {
+  var elem, instance;
+  elem = document.querySelector('#side-nav');
+  instance = M.Sidenav.getInstance(elem);
+  return instance.destroy();
+});
